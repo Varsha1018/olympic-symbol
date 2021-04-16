3f96f074a66c4c9499bdf3caa4a48fc14babc6a8
Varsha1018 <76804038+Varsha1018@users.noreply.github.com>
2021-04-16 17:35:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+color = "blue"; 
+
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 4;
+ctx.arc(200, 200, 60 ,0 , 2*Math.PI);
+ctx.stroke();
+
+color = "black"; 
+
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 4;
+ctx.arc(350, 200, 60 ,0 , 2*Math.PI);
+ctx.stroke();
+
+color = "red"; 
+
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 4;
+ctx.arc(500, 200, 60 ,0 , 2*Math.PI);
+ctx.stroke();
+
+color = "yellow"; 
+
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 4;
+ctx.arc(275, 250, 60 ,0 , 2*Math.PI);
+ctx.stroke();
+
+color = "green"; 
+
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 4;
+ctx.arc(420, 250, 60 ,0 , 2*Math.PI);
+ctx.stroke();
+
+
+
